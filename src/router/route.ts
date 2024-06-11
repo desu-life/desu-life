62937de28e26ef06384fb04ff834c9decbeadbdd
0ojixueseno0 <im0ojiang@outlook.com>
2024-06-11 13:59:18 +0800

    DELETE/UPDATE: 拆分用户面板项目

--- src/router/route.ts
+++ src/router/route.ts
@@ -4,33 +4,6 @@ const routes = [
     {
         path: "/",
         component: () => import("../views/Home.vue")
-    },
-    // {
-    //     path: "/login",
-    //     component: () => import("../views/Login.vue"),
-    //     meta: {
-    //       title: "登录"
-    //     }
-    // },
-    // {
-    //     path: "/register",
-    //     component: () => import("../views/Register.vue"),
-    //     meta: {
-    //       title: "注册"
-    //     }
-    // },
-    {
-      path: "/user",
-      redirect: "/user/index",
-      children: [
-        {
-          path: "index",
-          component: () => import("../views/User/Index.vue"),
-          meta: {
-            title: "用户中心"
-          }
-        }
-      ]
     }
 ]
 
