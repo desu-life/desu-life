1eb2012bf95ae2f856326d7434420648c4abb6fe
Jz0ojiang <hi@im0o.top>
2024-07-12 19:03:56 +0800

    UPDATE: 日本語サポート (i18n)

---
+++ public/device/ja/meowpad_v2_se.md
@@ -0,0 +1,30 @@
+# desu.life デバイス 交流グループ (Tencent QQ)
+
+**一群：722122663 | 二群：309702152**
+
+# Meowpad 通用設定ツール
+## ローカルダウンロード
+[win x64](https://desu.life/device/app/download/Meowpad_v2/MeowpadConfiguratorForV2_v1.0.2_winx64.zip)  | [macos](https://desu.life/device/app/download/Meowpad_v2/MeowpadConfiguratorForV2_v1.0.2_winx64.zip) | [linux deb](https://desu.life/device/app/download/Meowpad_v2/MeowpadConfiguratorForV2_v1.0.2_winx64.zip)
+
+*Meowpad V2および以降に発売された製品にのみ対応。
+
+# ファームウェアのダウンロード
+## Meowpad SE V2
+[1.0.0](https://desu.life/device/firmware/download/Meowpad_SE_v2/Meowpad_SE_v2_1.0.0.bin)
+
+## Meowpad V2
+
+[1.0.1](https://desu.life/device/firmware/download/Meowpad_v2/Meowpad_v2_1.0.1.bin)
+
+
+# Meowpad V1
+製品説明およびファームウェアアップグレードチュートリアル： [インフォメーションサイト](https://info.desu.life/?p=338)
+## Meowpad v1 設定ツール（0.3.1）
+[win x64](https://desu.life/device/app/download/Meowpad_v1/MeowpadConfigurator_0.3.1_x64_en-US.msi.zip)  | [macos](https://desu.life/device/app/download/Meowpad_v1/MeowpadConfigurator_0.3.1_macos-app.zip) | [linux deb](https://desu.life/device/app/download/Meowpad_v1/MeowpadConfigurator_0.3.1_amd64.deb.zip)
+
+## Meowpad v1 ファームウェア
+meowpad_v1.1_hsファームウェア (ホール効果リリース1.0.0パッチ23090101)： [ローカルダウンロード](https://desu.life/device/firmware/download/Meowpad_v1/meowpad_v1_app_hs_edition_1.0.0_ptach_23090101.bin)
+
+meowpad_v1.0ファームウェア (メカニカルベータ0.1.6)： [ローカルダウンロード](https://desu.life/device/firmware/download/Meowpad_v1/meowpad_v1_app_0.1.6.bin)
+
+meowpad_v1ファームウェアアップグレードキット (dfu-util)：[ローカルダウンロード](https://desu.life/device/firmware/download/Meowpad_v1/firmware_updater.zip)
---
+++ src/locales/ja.ts
@@ -0,0 +1,104 @@
+// 日本語
+
+export default {
+    lang: {
+      language: "ja",
+      languageName: "日本語",
+    },
+    page: {
+      // 首页
+      main: {
+        menu: {
+          options: {
+            info: "インフォメーション",
+            mail: "メール"
+          },
+          kanonbot: "Bot",
+          device: "Meowpad",
+          discord: "Discord",
+          support: "サポート",
+          about: "概要",
+        },
+      },
+      // QQ机器人
+      kanonbot: {
+        title: "KanonBot",
+        subtitle: "多プラットフォーム対応のosu!スコアボット",
+        description: [
+          "プロフィール、プレイ成績、pp+データの比較",
+          "大会のcost値、今日の新しいbpの確認、マップの推薦（std）",
+          "Kanonメダルシステム、カスタマイズ可能なosu!情報パネル画像"
+        ],
+        linkToDetail: "猫ちゃんはもっとできる！>>",
+        linkTooltip: "インフォメーションでKanonBot2022の使用ドキュメントを確認"
+      },
+      // 猫盘
+      device: {
+        // Meowpad v2
+        defaultModal: [
+          ["desu.lifeデバイスディスカッショングループ：722122663 {'|'} 309702152"],
+          "Meowpad V2：",
+          ["meowpad_v2コンフィギュレーター(1.0.1)：", "Windows x64", "Linux x64 (deb)", "Linux x64 (appimage)"],
+          ["meowpad_v2スタンダードエディションファームウェア (1.0.1)：", "ローカルダウンロード"],
+          "Meowpad V1：",
+          ["製品説明とファームウェアアップグレードチュートリアル：", "インフォメーションを確認"],
+          ["meowpad_v1コンフィギュレーター(0.3.1)：", "Windows x64", "Linux x64 (deb)", "MacOS x64"],
+          ["meowpad_v1.1_hsファームウェア (ホールエフェクトリリース1.0.0パッチ23090101)：", "ローカルダウンロード"],
+          ["meowpad_v1.0ファームウェア (メカニカルベータ0.1.6)：", "ローカルダウンロード"],
+          ["meowpad_v1ファームウェアアップグレードキット (dfu-util)：", "ローカルダウンロード"]
+        ],
+        v2: {
+          title: "Meowpad v2",
+          subtitle: "高性能磁軸小型キーボード",
+          benefits: [
+            "優れたハードウェア性能",
+            "0.125msの高速応答",
+            "多様なライティングモード",
+            "カスタマイズ可能なトリガー"
+          ],
+          linkToMarket: "こんなにいいの？私にも一つ！>>",
+          linkTooltip: "Meowpadのタオバオページを開く",
+          btnOpenDialog: "もう持ってる？ドライバーをダウンロード！>>",
+          btnTooltip: "ドライバープログラムをダウンロード",
+          modalTitle: "Meowpadドライバーをダウンロード",
+        },
+        v2Se: {
+          title: "Meowpad SE v2",
+          subtitle: "多シーン対応の磁軸小型キーボード",
+          benefits: [
+            "USB-Cポート",
+            "1msの高速応答",
+            "調整可能なバックライト",
+            "カスタマイズ可能なトリガー"
+          ],
+          linkToMarket: "こんなにいいの？私にも一つ！>>",
+          linkTooltip: "Meowpadのタオバオページを開く",
+          btnOpenDialog: "もう持ってる？ドライバーをダウンロード！>>",
+          btnTooltip: "ドライバープログラムをダウンロード",
+          modalTitle: "Meowpadドライバーをダウンロード",
+        },
+      },
+      // Discord
+      discord: {
+        label: "DESU.Lifeがあなたを招待します",
+        name: "Discordサーバー",
+        btn: "クリックして参加"
+      },
+      // 赞助
+      support: {
+        title: "DESU.Lifeをサポートする",
+        content: [
+          "DESU.Lifeの運営は皆さんのサポートなしでは成り立ちません",
+          "画面に表示される全ての仲間たちに感謝します。",
+          "ここには2023年12月までに記録されたすべてのサポートが記載されています。",
+          "すべてのサポート金額はサーバー、ドメイン、各種クラウドサービスの費用に充てられます。",
+          "しかし、様々な理由から、現在は新しいサポートを受け付けていません。",
+          "それでも、DESU.Lifeは皆さんと一緒に私たちの望む未来に向かって進んでいきます。",
+        ]
+      },
+      footer: {
+        topimgCopyRight: "トップ画像はランダムな画像ライブラリからのものであり、著作権は各作者に帰属します"
+      }
+    },
+  };
+  
